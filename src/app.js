9e4247d2e875b5965c734fff5098759576020d43
FaithIsWork11 <faithiswork@duck.com>
2024-07-02 12:39:12 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let card = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  let suit = ["♠ ", "♥ ", "♣", "♦"];
+  let randomCard = Math.floor(Math.random() * card.length);
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  if (randomSuit == 1 || randomSuit == 3) {
+    document.getElementById("card1").style.color = "red";
+    document.getElementById("card2").style.color = "red";
+  }
+  let newCard = document.querySelector("#card1");
+  newCard.innerHTML = suit[randomSuit];
+  let newCard2 = document.querySelector("#card2");
+  newCard2.innerHTML = suit[randomSuit];
+  let number = document.querySelector("#center");
+  number.innerHTML = card[randomCard];
+  //newCard.classList.remove('heart')
+  //newCard.classList.add(randomSuit);
 };
